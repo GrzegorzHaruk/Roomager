b4914028ecda436020b622178985a9934782d113
Gharucchi <gharucchi@gmail.com>
2021-03-30 22:25:35 +0200

    Added basic Roomager.Web project setup
    Added
    - initial Startup.cs configuration
    - initial folder structure
    - bootstrap library
    - _Layout, _ViewStart and _ViewImports cshtml files
    - HomeController with corresponding view

--- Roomager.Web/Startup.cs
+++ Roomager.Web/Startup.cs
@@ -16,6 +16,9 @@ namespace Roomager.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options=>options.EnableEndpointRouting=false);
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +29,15 @@ namespace Roomager.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
