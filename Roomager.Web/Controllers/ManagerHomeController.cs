14ec6a313a6e299483fb06030b58b8a64b5ec604
Gharucchi <gharucchi@gmail.com>
2021-04-20 22:12:26 +0200

    Added temporary look for application
    Added
    - Basic views for navigation and CRUD operations
    - Corresponding controllers
    - Custom css and pictures
    Switched bootstrap to 3.4.1 version

---
+++ Roomager.Web/Controllers/ManagerHomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Roomager.Web.Controllers
+{
+    public class ManagerHomeController : Controller
+    {
+        public IActionResult ManagerHomePage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Roomager.Web/Controllers/PaymentsManagerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Roomager.Web.Controllers
+{
+    public class PaymentsManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult CreateRecord()
+        {
+            return View();
+        }
+
+        public ViewResult RecordDetails()
+        {
+            return View();
+        }
+
+        public ViewResult EditRecord()
+        {
+            return View();
+        }
+
+        public ViewResult DeleteRecord()
+        {
+            return View();
+        }
+    }
+}
