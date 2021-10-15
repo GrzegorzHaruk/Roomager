4f578ab6d3b3b7862de9078f928f1f246b554655
Gharucchi <gharucchi@gmail.com>
2021-10-15 23:41:54 +0200

    Added Payment Calculator Service

---
+++ Roomager.Services/PaymentsServices/PaymentCalculatorService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Roomager.Data;
+
+namespace Roomager.Services.PaymentsServices
+{
+    public class PaymentCalculatorService
+    {
+        public decimal CalculateTotalPayment(decimal energyCost, decimal coldWaterCost, decimal hotWaterCost, decimal gasCost)
+        {
+            decimal totalPayment;
+
+            totalPayment = energyCost + coldWaterCost + hotWaterCost + gasCost;
+
+            return totalPayment;
+        }
+
+        public decimal CalculatePaymentPerPerson(int tenantsNumber, decimal payment)
+        {
+            decimal paymentPerPerson = 0m;
+
+            if (tenantsNumber > 0 & payment > 0)
+            {
+                paymentPerPerson = payment / tenantsNumber;
+            }
+
+            return paymentPerPerson;
+        }
+    }
+}
